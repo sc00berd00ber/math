3dc20465fe9bd95d9dd87aad213592ca408cfea2
Nat Tessema <natnaeltessema1@gmail.com>
2025-04-10 11:34:24 -0700

    1st update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double degrees = 90.0;
+        double answer = Math.sin(degrees);
+        System.out.println(answer);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        double value = 1234.567;
+        long wholeNumber = Math.round(value); // round up if fraction is 0.5 or greater
+        System.out.println(wholeNumber);
+        System.out.println(Math.ceil(value)); // round up no matter what
+        System.out.println(Math.floor(value));
     }
 }
